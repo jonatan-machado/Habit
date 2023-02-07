164231e9637d38a2b05fcf833232a23e5d3b6d9c
Jonatan Machado <jonatanmachado35@gmail.com>
2023-02-07 14:56:17 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Jonatan Machado on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
